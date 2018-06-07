4c13810f0ed067404fa9184adc68c0fd9b5a6d83
Rossa <IcyRossa@gmail.com>
2018-06-07 21:07:46 +0100

    Redux Store & Chrome Extension Setup

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const intialState = {
+  isAuthenicated: false,
+  user: {}
+};
+
+export default function(state = intialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
