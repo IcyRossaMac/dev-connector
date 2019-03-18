1eb6a2b9a26b954f4303e01e3d797d1c9bdda2b4
IcyRossaMac <IcyRossa@gmail.com>
2019-03-18 09:48:19 +0000

    CreateProfile Component & Route Complete Pt.51

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -34,7 +34,7 @@ const TextFieldGroup = ({
 
 TextFieldGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  palceholder: PropTypes.string,
+  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
