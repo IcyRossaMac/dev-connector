662270ab6a93ad5366e537d6fd6a85b9bd35b78d
IcyRossaMac <IcyRossa@gmail.com>
2019-03-18 09:29:48 +0000

    Private Route Setup complete Pt.50

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { clearCurrentProfile } from "./actions/profileActions";
+import {setCurrentUser, logoutUser} from "./actions/authActions";
+import {clearCurrentProfile} from "./actions/profileActions";
 
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store";
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -48,19 +50,21 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
             <div className="container">
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              </Switch>
             </div>
-            <Footer />
+            <Footer/>
           </div>
         </Router>
       </Provider>
-    );
+  );
   }
-}
+  }
 
-export default App;
+  export default App;
