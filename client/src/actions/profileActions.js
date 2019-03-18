ee9b4eafc46bd3d4bd53fd6b0ab76e394c81a06f
IcyRossaMac <IcyRossa@gmail.com>
2019-03-18 16:22:56 +0000

    Profile Actions Component & Delete Account Complete Pt.54

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS} from "./types";
+import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -25,6 +25,22 @@ export const createProfile = (profileData, history) => dispatch => {
       type: GET_ERRORS,
       payload: err.response.data
     }))
+};
+
+// Delete Account
+export const deleteAccount= () => dispatch => {
+  if(window.confirm('Are you sure? This can not be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res => dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      }))
+      .catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
+  }
 };
 
 // Profile loading
