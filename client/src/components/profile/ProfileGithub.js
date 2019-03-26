241ff2a17f7243e8d75a91689458631914facb1a
IcyRossaMac <IcyRossa@gmail.com>
2019-03-26 16:57:33 +0000

    Profile By Handle & Sub Components Pt.63, Profile Header Pt64 complete

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ProfileGithub extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TODO: Profile Github</h1>
+      </div>
+    );
+  }
+}
+
+// ProfileGithub.propTypes = {};
+
+export default ProfileGithub;
