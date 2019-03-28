5e72ca27109b3da343b20260bbec029601997995
IcyRossaMac <IcyRossa@gmail.com>
2019-03-28 17:12:10 +0000

    Posts & Post Form Component Pt.68 complete

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import PostFrom from './PostForm';
+import Spinner from '../common/Spinner';
+
+class Posts extends Component {
+  render() {
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostFrom/>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+Posts.propTypes = {};
+
+export default Posts;
