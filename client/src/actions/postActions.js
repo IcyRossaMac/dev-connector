5826ba8cd7e3c4ae1f58fc212fc55d430adfd7d3
IcyRossaMac <IcyRossa@gmail.com>
2019-03-29 14:32:55 +0000

    getPosts Action & PostFeed Component Pt.69 complete

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import {ADD_POST, GET_ERRORS} from "./types";
+import {ADD_POST, GET_POSTS, GET_ERRORS, POST_LOADING} from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -16,4 +16,28 @@ export const addPost = postData => dispatch => {
         type: GET_ERRORS,
         payload: err.response.data
       }))
+};
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      }))
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      }))
+};
+
+// Set Loading State
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  }
 };
